03d56b618922ad28549d9b6c43026e5e009a4bb7
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-12-02 20:48:18 +0200

    add nav

---
+++ src/components/hero/hero.js
@@ -0,0 +1,12 @@
+import React, {Component } from 'react';
+import { Link, Route, Prompt } from 'react-router-dom'
+
+export default class Hero extends Component {
+  render() {
+    return (
+        <section className="hero">
+
+        </section>
+    );
+  }
+};
