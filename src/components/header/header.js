c1a76c2a65be00568c8dc70f26123f9c08532e9f
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-12-20 08:49:52 +0200

    change files

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,24 +5,26 @@ import logo from '../../assets/img/logo.png'
 
 
 export default class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.anchorClick = this.anchorClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.anchorClick = this.anchorClick.bind(this);
+  }
 
-    anchorClick(event) {
-        document.location.replace(event.target.getAttribute('href'))
-    }
+  anchorClick(event) {
+    document.location.replace(event.target.getAttribute('href'))
+  }
 
-<<<<<<< HEAD
+
   render() {
     return (
         <section className="header">
           <header className='App-header'>
             <div className="logo__section">
-              <a href="#Home"><img src={logo} className='App-logo' alt='logo'/></a>
-              <h3><span className='logo'>Pro</span>Jex</h3>
-              <h2>One Page Business Theme</h2>
+              <div className="logo__wrap__desc">
+                <a href="#Home"><img src={logo} className='App-logo' alt='logo'/></a>
+                <h3 className="logo__title"><span className='logo'>Pro</span>Jex</h3>
+              </div>
+              <h2 className="logo-sub__title">One Page Business Theme</h2>
             </div>
             <nav className='nav'>
               <a onClick={this.anchorClick} href='#Home'>Home</a>
@@ -37,30 +39,4 @@ export default class Header extends Component {
     );
   }
 };
-=======
-    render() {
-        return (
-            <section className="header">
-                <header className='container App-header'>
-                    <div className="logo__section">
-                        <div className='logoBlock'>
-                            <a href="#Home"><img src={logo} className='App-logo' alt='logo'/></a>
-                            <h3><span className='logo'>Pro</span>Jex</h3>
-                        </div>
-                        <h2>One Page Business Theme</h2>
-                    </div>
-                    <nav className='nav'>
-                        <a className='open' onClick={this.anchorClick} href='#Home'>Home</a>
-                        <a onClick={this.anchorClick} href='#Services'>Services</a>
-                        <a onClick={this.anchorClick} href='#Team'>Our Team</a>
-                        <a onClick={this.anchorClick} href='#Portfolio'>Portfolio</a>
-                        <a onClick={this.anchorClick} href='#News'>News</a>
-                        <a onClick={this.anchorClick} href='#Contact'>Contact Us</a>
-                    </nav>
-                </header>
-            </section>
 
-        );
-    }
-};
->>>>>>> 835ae907b87cff57d81ec1b6b99f063ff09e5cac
